435a8c32a7ee2c1d0c9ea7e7e21ffb77a2b27f46
SergeevSS55 <yourhonor55@mail.ru>
2024-07-08 13:09:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 class House:
-    def __init__(self, name, number_of_floors):
+    def __init__(self, name, number_of_floors): # Инициализируем атрибуты объекта
         self.name = name
         self.number_of_floors = number_of_floors
 
 
-    def go_to(self, new_floor):
+    def go_to(self, new_floor): # Проверка на допустимость этажа
         if new_floor > self.number_of_floors or new_floor < 1:
             print("Такого этажа не существует")
         for i in range(new_floor):
@@ -17,5 +17,6 @@ h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
 h2.go_to(10)
+
 
 
